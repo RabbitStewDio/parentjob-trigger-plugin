d25a7cd86bb69234a1e2df91eba3e07e37f98d86
Thomas Morgner <tmorgner@pentaho.com>
2014-07-10 15:24:56 +0100

    Add missing metadata and constructor annotation to make it work.

--- src/main/java/org/jenkinsci/plugins/sample/ParentProjectTrigger.java
+++ src/main/java/org/jenkinsci/plugins/sample/ParentProjectTrigger.java
@@ -1,22 +1,27 @@
 package org.jenkinsci.plugins.sample;
 
 import antlr.ANTLRException;
+import hudson.Extension;
 import hudson.model.AbstractProject;
 import hudson.triggers.SCMTrigger;
 import hudson.triggers.Trigger;
 import hudson.triggers.TriggerDescriptor;
+import hudson.util.SequentialExecutionQueue;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
 
 public class ParentProjectTrigger extends SCMTrigger {
 
   private boolean onlyDirectReferences;
 
-
-  public ParentProjectTrigger(final String cronTabSpec) throws ANTLRException {
+  @DataBoundConstructor
+  public ParentProjectTrigger(final String cronTabSpec, final boolean onlyDirectReferences) throws ANTLRException {
     super(cronTabSpec);
+    this.onlyDirectReferences = onlyDirectReferences;
   }
 
   @Override
@@ -39,7 +44,25 @@ public class ParentProjectTrigger extends SCMTrigger {
     return onlyDirectReferences;
   }
 
-  public void setOnlyDirectReferences(boolean onlyDirectReferences) {
+  public void setOnlyDirectReferences(final boolean onlyDirectReferences) {
     this.onlyDirectReferences = onlyDirectReferences;
   }
+
+  @Extension
+  @SuppressWarnings("unused")
+  public static class ParentProjectTriggerDescriptor extends DescriptorImpl {
+
+    private final transient SequentialExecutionQueue queue = new SequentialExecutionQueue(Executors.newSingleThreadExecutor());
+
+    @Override
+    public String getDisplayName() {
+      return "[ParentProjectTrigger] - Monitor this and all parent jobs for SCM changes";
+    }
+
+    @Override
+    public String getHelpFile() {
+      return null;
+    }
+  }
+
 }
